da1c1dffd215ddde772dc18071593f0b7addf186
Raul Ramirez <raul.ramtap@gmail.com>
2024-05-27 17:56:54 -0400

    Initial commit

---
+++ test/tqsft-openresty.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TqsftOpenresty from '../lib/tqsft-openresty-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tqsft-openresty-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TqsftOpenresty.TqsftOpenrestyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
